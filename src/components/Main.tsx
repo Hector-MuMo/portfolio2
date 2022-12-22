2c50cc566180485391930cff4c55d96029091e4e
Hector Muñoz M <hectormm@hobbio.co>
2022-12-22 12:56:08 -0600

    home section | mobile

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Home from './Home'
+import { slide as Menu } from 'react-burger-menu'
+import SocialMediaMenu from './SocialMediaMenu'
 
 const Main = () => {
     return (
-        <div>Se comienza el proyecto</div>
+        <div >
+            <Menu>
+                <a id="home" href="/">Home</a>
+                <a id="profile" href="/">Profile</a>
+                <a id="skills" href="/">Skills</a>
+                <a id="portfolio" href="/">Portfolio</a>
+                <a id="Contact" href="/">Contact</a>
+            </Menu>
+            <SocialMediaMenu />
+            <Home />
+        </div>
     )
 }
 
