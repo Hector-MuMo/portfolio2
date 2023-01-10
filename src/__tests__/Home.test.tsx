74629450fc29e5dcbd1e30b94ad34d400158da23
Hector Muñoz M <hectormm@hobbio.co>
2023-01-10 16:16:41 -0600

    fix Home component test

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Main from '../components/Main';
+import Home from '../components/Home';
 
 test("check start project string", () => {
-    render(<Main />)
-    const text = screen.getByText("Home")
+    render(<Home />)
+    const text = screen.getByText("Perfil")
     expect(text).toBeInTheDocument()
 })
